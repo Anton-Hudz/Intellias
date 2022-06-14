f97fee8055f5a4cf84fbfc10ec1602b05388dc8e
Anton Hudz <pat280885@mail.ru>
2022-06-14 22:13:00 +0300

    Correction variables naming+add sub variable

--- main.go
+++ main.go
@@ -13,13 +13,20 @@ var (
 )
 
 func main() {
-	resultFirstQuestion := applePrice*float32(amountOfApples) + pearPrice*float32(amountOfPears)
+	applesCost := applePrice * float32(amountOfApples)
+	pearsCost := pearPrice * float32(amountOfPears)
+	resultFirstQuestion := applesCost + pearsCost
 	fmt.Println("1. Скільки грошей треба витратити, щоб купити", amountOfApples, "яблук та", amountOfPears, "груш?", "вiдповiдь:",
 		resultFirstQuestion, "грн.")
+
 	resultSecondQuestion := int(myCash / pearPrice)
 	fmt.Println("2. Скільки груш ми можемо купити?", "вiдповiдь:", resultSecondQuestion, "шт.")
+
 	resultThirdQuestion := int(myCash / applePrice)
 	fmt.Println("3. Скільки яблук ми можемо купити?", "вiдповiдь:", resultThirdQuestion, "шт.")
-	resultFourthQuestion := myCash >= float32(pearsCount)*pearPrice+float32(applesCount)*applePrice
+
+	pearsCountCost := float32(pearsCount) * pearPrice
+	applesCountCost := float32(applesCount) * applePrice
+	resultFourthQuestion := myCash >= pearsCountCost+applesCountCost
 	fmt.Println("4. Чи ми можемо купити", pearsCount, "груші та", applesCount, "яблука?", "вiдповiдь:", resultFourthQuestion)
 }
